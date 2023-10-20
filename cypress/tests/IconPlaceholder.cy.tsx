62762c383476c0c1a25b37fed5577451c99a7875
KurtXiaoZ <kurtxiaoz@gmail.com>
2023-10-20 15:32:11 -0400

    IconPlaceholder tests

---
+++ cypress/tests/IconPlaceholder.cy.tsx
@@ -0,0 +1,54 @@
+import { IconPlaceholder } from "../../src/components/IconPlaceholder";
+import { ICON_BASE_STYLE, ICON_CONTAINER_BASE_STYLE } from '../../src/lib/styles';
+import { verifyComputedStyle } from "../lib/util";
+
+describe('tests for IconPlaceholder', () => {
+    describe('all elements should be rendered correctly', () => {
+        it('all elements should be in the document', () => {
+            cy.mount(<IconPlaceholder />);
+            cy.get('[data-testid=ip-iconPlaceholderContainer]').should('exist');
+            cy.get('[data-testid=ip-iconPlaceholderContainer]').should('not.be.visible');
+            cy.get('[data-testid=ip-iconPlaceholder]').should('exist');
+            cy.get('[data-testid=ip-iconPlaceholder]').should('not.be.visible');
+        });
+
+        it('all elements should have the correct base style', () => {
+            cy.mount(<IconPlaceholder />);
+            verifyComputedStyle('[data-testid=ip-iconPlaceholderContainer]', {
+                ...ICON_CONTAINER_BASE_STYLE,
+                visibility: 'hidden'
+            });
+            verifyComputedStyle('[data-testid=ip-iconPlaceholder]', ICON_BASE_STYLE({ hex: '#000000' }), {
+                color: ['rgb(0, 0, 0)']
+            });
+        });
+    });
+
+    describe('test props', () => {
+        it('test styles -- iconContainer', () => {
+            cy.mount(<IconPlaceholder styles={{ iconContainer: baseStyle => ({ ...baseStyle, border: '1px solid red' }) }}/>);
+            verifyComputedStyle('[data-testid=ip-iconPlaceholderContainer]', {
+                ...ICON_CONTAINER_BASE_STYLE,
+                visibility: 'hidden'
+            }, {
+                border: ['1px solid red']
+            });
+        });
+
+        it('test styles -- icon', () => {
+            cy.mount(<IconPlaceholder styles={{ icon: baseStyle => ({ ...baseStyle, border: '1px solid red' }) }}/>);
+            verifyComputedStyle('[data-testid=ip-iconPlaceholder]', ICON_BASE_STYLE({ hex: '#000000' }), {
+                border: ['1px solid red'],
+                color: ['rgb(0, 0, 0)']
+            });
+        });
+
+        it('test minimizeHeight', () => {
+            cy.mount(<IconPlaceholder minimizeHeight={true}/>);
+            verifyComputedStyle('[data-testid=ip-iconPlaceholderContainer]', {
+                ...ICON_CONTAINER_BASE_STYLE,
+                height: '1px'
+            });
+        });
+    });
+});
--- src/components/IconPlaceholder/index.tsx
+++ src/components/IconPlaceholder/index.tsx
@@ -1,38 +1,34 @@
-import { ForwardedRef, forwardRef } from 'react';
 import { ICON_BASE_STYLE, ICON_CONTAINER_BASE_STYLE } from '../../lib/styles';
 import { IconProps } from './types';
 import { MATERIAL_ICONS } from '../../assets/materialIcons';
 
-export const IconPlaceholder = forwardRef(
-  (props: IconProps, iconsContainerRef: ForwardedRef<HTMLDivElement | null>) => {
-    const { styles, minimizeHeight = false } = props || {};
+export const IconPlaceholder = (props: IconProps) => {
+  const { styles, minimizeHeight = false } = props || {};
+  const { iconContainer, icon: iconStyle } = styles || {};
+  const iconContainerStyle = iconContainer
+    ? iconContainer(ICON_CONTAINER_BASE_STYLE)
+    : ICON_CONTAINER_BASE_STYLE;
 
-    const { iconContainer, icon: iconStyle } = styles || {};
-
-    const iconContainerStyle = iconContainer
-      ? iconContainer(ICON_CONTAINER_BASE_STYLE)
-      : ICON_CONTAINER_BASE_STYLE;
-
-    return (
+  return (
+    <div
+      style={{
+        ...iconContainerStyle,
+        visibility: 'hidden',
+        ...(minimizeHeight && { height: '1px' }),
+      }}
+      data-testid="ip-iconPlaceholderContainer"
+    >
       <div
-        style={{
-          ...iconContainerStyle,
-          visibility: 'hidden',
-          ...(minimizeHeight && { height: '1px' }),
-        }}
-        data-testid="ip-iconPlaceholderContainer"
+        className={'material-icons'}
+        style={
+          iconStyle
+            ? iconStyle(ICON_BASE_STYLE({ hex: '#000000' }))
+            : ICON_BASE_STYLE({ hex: '#000000' })
+        }
+        data-testid='ip-iconPlaceholder'
       >
-        <div
-          className={'material-icons'}
-          style={
-            iconStyle
-              ? iconStyle(ICON_BASE_STYLE({ hex: '#000000' }))
-              : ICON_BASE_STYLE({ hex: '#000000' })
-          }
-        >
-          {MATERIAL_ICONS[0]}
-        </div>
+        {MATERIAL_ICONS[0]}
       </div>
-    );
-  },
-);
+    </div>
+  );
+};
